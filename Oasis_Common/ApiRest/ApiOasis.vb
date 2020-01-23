d2609420c97885aff7e729fd9b18e38e71bf37c8
devdm <devdm@24132e46-67f6-c34f-8568-d0b77f9b99e6>
2020-01-23 16:54:44 +0000

    git-svn-id: https://ns3119889.ip-51-38-181.eu:8443/svn/oasis@48 24132e46-67f6-c34f-8568-d0b77f9b99e6

---
+++ Oasis_Common/ApiRest/ApiOasis.vb
@@ -0,0 +1,48 @@
+﻿Imports System.Net
+Imports System.Net.Http
+Imports System.Net.Http.Headers
+Imports System.Threading.Tasks
+
+Public Class ApiOasis
+
+    Property serveurDomain As String
+
+    Dim request As HttpWebRequest
+    Dim dictLoginPwd As Dictionary(Of String, String)
+    Dim loginRequest As LoginRequest
+    Shared client As HttpClient = New HttpClient()
+
+    Public Sub New(ByVal _serveurDomain As String)
+        serveurDomain = _serveurDomain
+        'isProxyManuel = _isProxyManuel
+        'proxyUrl = _proxyUrl
+        'proxyPort = _proxyPort
+        'proxyLogin = _proxyLogin
+        'proxyPassword = _proxyPassword
+    End Sub
+
+    Public Function loginRest(loginRquest As LoginRequest) As String
+        Dim str = login(serveurDomain, loginRequest).GetAwaiter().GetResult()
+        Return str
+    End Function
+
+    Private Shared Async Function login(serveurDomain As String, ByVal loginRequest As LoginRequest) As Task(Of String)
+        initHttp(serveurDomain)
+        Dim response As HttpResponseMessage = Await client.PostAsJsonAsync("/login", loginRequest)
+        response.EnsureSuccessStatusCode()
+        Dim str = Await response.Content.ReadAsAsync(Of String)()
+        Return str
+    End Function
+
+    Private Shared Sub initHttp(serveurDomain As String)
+        client.BaseAddress = New Uri("https://" + serveurDomain + "/api")
+        client.DefaultRequestHeaders.Accept.Clear()
+        client.DefaultRequestHeaders.Accept.Add(New MediaTypeWithQualityHeaderValue("application/json"))
+
+    End Sub
+
+
+
+
+
+End Class
