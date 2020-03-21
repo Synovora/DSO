4cd3f8b914e474bf2838122412875932a7cb7760
b.gambet <b.gambet@24132e46-67f6-c34f-8568-d0b77f9b99e6>
2020-03-21 14:17:17 +0000

    git-svn-id: https://ns3119889.ip-51-38-181.eu:8443/svn/oasis@208 24132e46-67f6-c34f-8568-d0b77f9b99e6

--- oasis/Form/Theriaque/RadFATCListe.vb
+++ oasis/Form/Theriaque/RadFATCListe.vb
@@ -1,6 +1,17 @@
-﻿Imports Telerik.WinControls.UI
+﻿Imports System.Collections.Specialized
+Imports Telerik.WinControls.UI
 
 Public Class RadFATCListe
+    Private _SelectedSpecialiteId As Long
+
+    Public Property SelectedSpecialiteId As Long
+        Get
+            Return _SelectedSpecialiteId
+        End Get
+        Set(value As Long)
+            _SelectedSpecialiteId = value
+        End Set
+    End Property
 
     Dim theriaqueDao As New TheriaqueDao
 
@@ -17,29 +28,60 @@ Public Class RadFATCListe
             Cursor.Current = Cursors.WaitCursor
             '1 - Recherche spécialités virtuelles en nom partiel
             Dim dt As DataTable
-            Dim NomSpecialite As String = "%" & RadTxtSpecialite.Text & "%"
-            dt = ChargementSpecialiteByNomSpecialite(NomSpecialite, TheriaqueDao.EnumMonoVir.VIRTUEL)
+            Dim NomSpecialite As String = RadTxtSpecialite.Text & "%"
+            dt = GetSpecialiteByNomSpecialite(NomSpecialite, TheriaqueDao.EnumMonoVir.VIRTUEL)
             If dt.Rows.Count > 0 Then
-                ChargementSpecialite(dt)
+                ChargementSpecialite(dt, True)
             Else
                 '2 - Recherche spécialités classiques en nom complet
-                NomSpecialite = "%" & RadTxtSpecialite.Text & " %"
-                dt = ChargementSpecialiteByNomSpecialite(NomSpecialite, TheriaqueDao.EnumMonoVir.CLASSIQUE)
+                NomSpecialite = RadTxtSpecialite.Text & " %"
+                dt = GetSpecialiteByNomSpecialite(NomSpecialite, TheriaqueDao.EnumMonoVir.CLASSIQUE)
                 If dt.Rows.Count > 0 Then
-                    ChargementSpecialite(dt)
+                    ChargementSpecialite(dt, True)
                 Else
                     '3 - Recherche spécialités classiques en nom partiel
-                    NomSpecialite = "%" & RadTxtSpecialite.Text & "%"
-                    dt = ChargementSpecialiteByNomSpecialite(NomSpecialite, TheriaqueDao.EnumMonoVir.CLASSIQUE)
-                    ChargementSpecialite(dt)
+                    NomSpecialite = RadTxtSpecialite.Text & "%"
+                    dt = GetSpecialiteByNomSpecialite(NomSpecialite, TheriaqueDao.EnumMonoVir.CLASSIQUE)
+                    RadGridViewSpe.Rows.Clear()
+                    RadGridViewSpe.FilterDescriptors.Clear()
+                    If dt.Rows.Count > 0 Then
+                        'Récupération des ATC des spécialités classiques en nom partiel
+                        Dim ResultatOk As Boolean = False
+                        Dim ListATC As New StringCollection
+                        Dim rowCount As Integer = dt.Rows.Count - 1
+                        For i = 0 To rowCount Step 1
+                            If ListATC.Contains(dt.Rows(i)("SP_CATC_CODE_FK")) = False Then
+                                ListATC.Add(dt.Rows(i)("SP_CATC_CODE_FK"))
+                            End If
+                        Next
+                        Dim EnumeratorATC As StringEnumerator = ListATC.GetEnumerator()
+                        While EnumeratorATC.MoveNext()
+                            Dim CodeATC As String = EnumeratorATC.Current.ToString
+                            dt = theriaqueDao.getSpecialiteByArgument(CodeATC, TheriaqueDao.EnumGetSpecialite.CLASSE_ATC, TheriaqueDao.EnumMonoVir.VIRTUEL)
+                            If dt.Rows.Count > 0 Then
+                                ResultatOk = True
+                            End If
+                            ChargementSpecialite(dt, False)
+                        End While
+
+                        'Si pas de correspondance ATC entre Virtuel et classique en nom pariel, on affiche les classiques en nom partiel
+                        If ResultatOk = False Then
+                            NomSpecialite = RadTxtSpecialite.Text & "%"
+                            dt = GetSpecialiteByNomSpecialite(NomSpecialite, TheriaqueDao.EnumMonoVir.CLASSIQUE)
+                            ChargementSpecialite(dt, True)
+                        End If
+                    Else
+                        LblOccurrencesLues.Text = "0 occurrence correspondant aux critères de recherche"
+                    End If
                 End If
             End If
             Cursor.Current = Cursors.Default
         Else
-            MessageBox.Show("Vous devez saisir au moins 3 caractères pour lancer cette option de recherche de médicaments !")
+            MessageBox.Show("Vous devez saisir au moins 3 caractères pour lancer cette option de recherche des médicaments !")
         End If
     End Sub
 
+    'Chargement du Grid affichant la classe thérapeutique de niveau 1
     Private Sub ChargementATC1()
         RadGridViewATC1.Rows.Clear()
         Dim dt As DataTable
@@ -62,6 +104,7 @@ Public Class RadFATCListe
         End If
     End Sub
 
+    'Chargement du Grid affichant la classe thérapeutique de niveau 2
     Private Sub ChargementATC2(CodeATC As String)
         RadGridViewATC2.Rows.Clear()
         Dim dt As DataTable
@@ -84,6 +127,7 @@ Public Class RadFATCListe
         End If
     End Sub
 
+    'Chargement du Grid affichant la classe thérapeutique de niveau 3
     Private Sub ChargementATC3(CodeATC As String)
         RadGridViewATC3.Rows.Clear()
         Dim dt As DataTable
@@ -106,6 +150,7 @@ Public Class RadFATCListe
         End If
     End Sub
 
+    'Chargement du Grid affichant la classe thérapeutique de niveau 4
     Private Sub ChargementATC4(CodeATC As String)
         RadGridViewATC4.Rows.Clear()
         Dim dt As DataTable
@@ -128,6 +173,7 @@ Public Class RadFATCListe
         End If
     End Sub
 
+    'Lancement de l'affichage du Grid affichant la classe thérapeutique de niveau 2 depuis le niveau 1
     Private Sub MasterTemplate_Click(sender As Object, e As EventArgs) Handles RadGridViewATC1.Click
         If RadGridViewATC1.CurrentRow IsNot Nothing Then
             Dim aRow As Integer = Me.RadGridViewATC1.Rows.IndexOf(Me.RadGridViewATC1.CurrentRow)
@@ -140,6 +186,7 @@ Public Class RadFATCListe
         End If
     End Sub
 
+    'Lancement de l'affichage du Grid affichant la classe thérapeutique de niveau 3 depuis le niveau 2
     Private Sub RadGridViewATC2_Click(sender As Object, e As EventArgs) Handles RadGridViewATC2.Click
         If RadGridViewATC2.CurrentRow IsNot Nothing Then
             Dim aRow As Integer = Me.RadGridViewATC2.Rows.IndexOf(Me.RadGridViewATC2.CurrentRow)
@@ -151,6 +198,7 @@ Public Class RadFATCListe
         End If
     End Sub
 
+    'Lancement de l'affichage du Grid affichant la classe thérapeutique de niveau 4 depuis le niveau 3
     Private Sub RadGridViewATC3_Click(sender As Object, e As EventArgs) Handles RadGridViewATC3.Click
         If RadGridViewATC3.CurrentRow IsNot Nothing Then
             Dim aRow As Integer = Me.RadGridViewATC3.Rows.IndexOf(Me.RadGridViewATC3.CurrentRow)
@@ -161,40 +209,41 @@ Public Class RadFATCListe
         End If
     End Sub
 
-    'Liste des médicaments ATC niveau 2
+    'Bouton d'appel de l'affichage de la Liste des médicaments depuis la classe thérapeutique de niveau 2
     Private Sub RadBtnSpec1_Click(sender As Object, e As EventArgs) Handles RadBtnSpec2.Click
         If RadGridViewATC2.CurrentRow IsNot Nothing Then
             Dim aRow As Integer = Me.RadGridViewATC2.Rows.IndexOf(Me.RadGridViewATC2.CurrentRow)
             If aRow >= 0 Then
                 Dim CodeAtc As String = RadGridViewATC2.Rows(aRow).Cells("catc_code_pk").Value
-                ChargementSpecialiteByATC(CodeAtc)
+                GetSpecialiteByATC(CodeAtc)
             End If
         End If
     End Sub
 
-    'Liste des médicaments ATC niveau 3
+    'Bouton d'appel de l'affichage de la Liste des médicaments depuis la classe thérapeutique de niveau 3
     Private Sub RadBtnSpec2_Click(sender As Object, e As EventArgs) Handles RadBtnSpec3.Click
         If RadGridViewATC3.CurrentRow IsNot Nothing Then
             Dim aRow As Integer = Me.RadGridViewATC3.Rows.IndexOf(Me.RadGridViewATC3.CurrentRow)
             If aRow >= 0 Then
                 Dim CodeAtc As String = RadGridViewATC3.Rows(aRow).Cells("catc_code_pk").Value
-                ChargementSpecialiteByATC(CodeAtc)
+                GetSpecialiteByATC(CodeAtc)
             End If
         End If
     End Sub
 
-    'Liste des médicaments ATC niveau 4
+    'Bouton d'appel de l'affichage de la Liste des médicaments depuis la classe thérapeutique de niveau 4
     Private Sub RadBtnSpec4_Click(sender As Object, e As EventArgs) Handles RadBtnSpec4.Click
         If RadGridViewATC4.CurrentRow IsNot Nothing Then
             Dim aRow As Integer = Me.RadGridViewATC4.Rows.IndexOf(Me.RadGridViewATC4.CurrentRow)
             If aRow >= 0 Then
                 Dim CodeAtc As String = RadGridViewATC4.Rows(aRow).Cells("catc_code_pk").Value
-                ChargementSpecialiteByATC(CodeAtc)
+                GetSpecialiteByATC(CodeAtc)
             End If
         End If
     End Sub
 
-    Private Sub ChargementSpecialiteByATC(CodeAtc As String)
+    'Appel API Thériaque : recherche spécialité par classe thérapeutique
+    Private Sub GetSpecialiteByATC(CodeAtc As String)
         Cursor.Current = Cursors.WaitCursor
 
         Dim Monovir As Integer = GetMonovir()
@@ -202,20 +251,24 @@ Public Class RadFATCListe
 
         Dim dt As DataTable
         dt = theriaqueDao.getSpecialiteByArgument(CodeAtc, TheriaqueDao.EnumGetSpecialite.CLASSE_ATC, Monovir)
-        ChargementSpecialite(dt)
+        ChargementSpecialite(dt, True)
 
         Cursor.Current = Cursors.Default
     End Sub
 
-    Private Function ChargementSpecialiteByNomSpecialite(NomSpecialite As String, Monovir As Integer) As DataTable
+    'Appel API Thériaque : recherche spécialité par nom de spécialité
+    Private Function GetSpecialiteByNomSpecialite(NomSpecialite As String, Monovir As Integer) As DataTable
         Dim dt As DataTable
         dt = theriaqueDao.getSpecialiteByArgument(NomSpecialite, TheriaqueDao.EnumGetSpecialite.NOM_SPECIALITE, Monovir)
         Return dt
     End Function
 
-    Private Sub ChargementSpecialite(dt As DataTable)
-        RadGridViewSpe.Rows.Clear()
-        RadGridViewSpe.FilterDescriptors.Clear()
+    'Chargement du Grid affichant les spécialités
+    Private Sub ChargementSpecialite(dt As DataTable, ClearRows As Boolean)
+        If ClearRows = True Then
+            RadGridViewSpe.Rows.Clear()
+            RadGridViewSpe.FilterDescriptors.Clear()
+        End If
 
         Dim NombreOccurrencesLues As Integer = dt.Rows.Count
         If NombreOccurrencesLues > 1 Then
@@ -246,6 +299,7 @@ Public Class RadFATCListe
         End If
     End Sub
 
+    'Récupération de la valeur des boutons radio (virtuel / classique)
     Private Function GetMonovir() As Integer
         Dim Monovir As Integer
 
@@ -258,9 +312,15 @@ Public Class RadFATCListe
         Return Monovir
     End Function
 
-
-    Private Sub RadBtnAbandon_Click(sender As Object, e As EventArgs) Handles RadBtnAbandon.Click
-        Close()
+    'Sélection d'une spécialité, renvoi de la valeur de la clé Thériaque
+    Private Sub RadGridViewSpe_DoubleClick(sender As Object, e As EventArgs) Handles RadGridViewSpe.DoubleClick
+        If RadGridViewSpe.CurrentRow IsNot Nothing Then
+            Dim aRow As Integer = Me.RadGridViewSpe.Rows.IndexOf(Me.RadGridViewSpe.CurrentRow)
+            If aRow >= 0 Then
+                Dim SpecialiteId As Long = RadGridViewSpe.Rows(aRow).Cells("SP_CODE_SQ_PK").Value
+                SelectedSpecialiteId = SpecialiteId
+            End If
+        End If
     End Sub
 
     Private Sub MasterTemplate_CellFormatting(sender As Object, e As Telerik.WinControls.UI.CellFormattingEventArgs) Handles RadGridViewSpe.CellFormatting
@@ -268,4 +328,10 @@ Public Class RadFATCListe
             e.CellElement.ToolTipText = e.CellElement.Text
         End If
     End Sub
+
+    'Sortie
+    Private Sub RadBtnAbandon_Click(sender As Object, e As EventArgs) Handles RadBtnAbandon.Click
+        Close()
+    End Sub
+
 End Class
