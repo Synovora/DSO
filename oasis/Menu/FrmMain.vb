fd7634d2805b914bf4cecdbab0f715c16ae5739c
devdm <devdm@24132e46-67f6-c34f-8568-d0b77f9b99e6>
2020-04-04 10:25:56 +0000

    git-svn-id: https://ns3119889.ip-51-38-181.eu:8443/svn/oasis@260 24132e46-67f6-c34f-8568-d0b77f9b99e6

--- oasis/Menu/FrmMain.vb
+++ oasis/Menu/FrmMain.vb
@@ -4,52 +4,92 @@ Imports Oasis_Common
 Public Class FrmMain
     Private Sub RadTileElementUtilisateur_Click(sender As Object, e As EventArgs) Handles RadTileElementUtilisateur.Click
         Me.Cursor = Cursors.WaitCursor
-        Using frmUtilisateurListe As New FrmUtilisateurListe
-            frmUtilisateurListe.ShowDialog()
-        End Using
-        Me.Cursor = Cursors.Default
+        Me.Enabled = False
+        Try
+            Using frm As New FrmUtilisateurListe
+                frm.ShowDialog()
+            End Using
+        Catch err As Exception
+            MsgBox(err.Message())
+        Finally
+            Me.Enabled = True
+            Me.Cursor = Cursors.Default
+        End Try
     End Sub
 
     Private Sub RadTileElementAPropos_Click(sender As Object, e As EventArgs) Handles RadTileElementAPropos.Click
         Me.Cursor = Cursors.WaitCursor
-        Using frm As New FrmAbout
-            frm.ShowDialog()
-        End Using
-        Me.Cursor = Cursors.Default
+        Me.Enabled = False
+        Try
+            Using frm As New FrmAbout
+                frm.ShowDialog()
+            End Using
+        Catch err As Exception
+            MsgBox(err.Message())
+        Finally
+            Me.Enabled = True
+            Me.Cursor = Cursors.Default
+        End Try
     End Sub
 
     Private Sub RadTileElement1_Click(sender As Object, e As EventArgs) Handles RadTileElement1.Click
         Me.Cursor = Cursors.WaitCursor
-        Using VFMUniteSanitaire As New FmUniteSanitaire
-            VFMUniteSanitaire.ShowDialog() 'Modal
-        End Using
-        Me.Cursor = Cursors.Default
+        Me.Enabled = False
+        Try
+            Using frm As New FmUniteSanitaire
+                frm.ShowDialog()
+            End Using
+        Catch err As Exception
+            MsgBox(err.Message())
+        Finally
+            Me.Enabled = True
+            Me.Cursor = Cursors.Default
+        End Try
     End Sub
 
     Private Sub RadTileDRCORC_Click(sender As Object, e As EventArgs) Handles RadTileDRCORC.Click
         Me.Cursor = Cursors.WaitCursor
-        Using vFDRCListe As New RadFDrcListe
-            vFDRCListe.UtilisateurConnecte = userLog
-            vFDRCListe.ShowDialog() 'Modal
-        End Using
-        Me.Cursor = Cursors.Default
+        Me.Enabled = False
+        Try
+            Using frm As New RadFDrcListe
+                frm.ShowDialog()
+            End Using
+        Catch err As Exception
+            MsgBox(err.Message())
+        Finally
+            Me.Enabled = True
+            Me.Cursor = Cursors.Default
+        End Try
     End Sub
 
     Private Sub RadTileRefMed_Click(sender As Object, e As EventArgs) Handles RadTileRefMed.Click
         Me.Cursor = Cursors.WaitCursor
-        Using vForm6 As New FMedocListe
-            vForm6.ShowDialog() 'Modal
-        End Using
-        Me.Cursor = Cursors.Default
+        Me.Enabled = False
+        Try
+            Using frm As New FMedocListe
+                frm.ShowDialog()
+            End Using
+        Catch err As Exception
+            MsgBox(err.Message())
+        Finally
+            Me.Enabled = True
+            Me.Cursor = Cursors.Default
+        End Try
     End Sub
 
     Private Sub RadTileElementRepublication_Click(sender As Object, e As EventArgs) Handles RadTileElementRepublication.Click
         Me.Cursor = Cursors.WaitCursor
-        Using vFAntecedentOccultesListe As New FAntecedentOccultesListe
-            vFAntecedentOccultesListe.UtilisateurConnecte = userLog
-            vFAntecedentOccultesListe.ShowDialog() 'Modal
-        End Using
-        Me.Cursor = Cursors.Default
+        Me.Enabled = False
+        Try
+            Using frm As New FAntecedentOccultesListe
+                frm.ShowDialog()
+            End Using
+        Catch err As Exception
+            MsgBox(err.Message())
+        Finally
+            Me.Enabled = True
+            Me.Cursor = Cursors.Default
+        End Try
     End Sub
 
     Private Sub RadTileDRCNouveau_Click(sender As Object, e As EventArgs) Handles RadTileDRCNouveau.Click
@@ -60,10 +100,17 @@ Public Class FrmMain
 
     Private Sub RadTileMedicament_Click(sender As Object, e As EventArgs) Handles RadTileMedicament.Click
         Me.Cursor = Cursors.WaitCursor
-        Using vFrmMedocListe As New FMedocListe
-            vFrmMedocListe.ShowDialog()
-        End Using
-        Me.Cursor = Cursors.Default
+        Me.Enabled = False
+        Try
+            Using frm As New FMedocListe
+                frm.ShowDialog()
+            End Using
+        Catch err As Exception
+            MsgBox(err.Message())
+        Finally
+            Me.Enabled = True
+            Me.Cursor = Cursors.Default
+        End Try
 
     End Sub
 
@@ -72,9 +119,18 @@ Public Class FrmMain
     End Sub
 
     Private Sub RadTileROR_Click(sender As Object, e As EventArgs) Handles RadTileROR.Click
-        Using vRadFRorListe As New RadFRorListe
-            vRadFRorListe.ShowDialog()
-        End Using
+        Me.Cursor = Cursors.WaitCursor
+        Me.Enabled = False
+        Try
+            Using frm As New RadFRorListe
+                frm.ShowDialog()
+            End Using
+        Catch err As Exception
+            MsgBox(err.Message())
+        Finally
+            Me.Enabled = True
+            Me.Cursor = Cursors.Default
+        End Try
     End Sub
 
     Private Sub RadTileElementTemplateSE_Click(sender As Object, e As EventArgs) Handles RadTileElementTemplateSE.Click
