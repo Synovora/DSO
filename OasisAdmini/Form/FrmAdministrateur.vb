190872ae135277ac06551fe9a78f61b400aeedeb
devdm <devdm@24132e46-67f6-c34f-8568-d0b77f9b99e6>
2020-04-27 15:45:38 +0000

    git-svn-id: https://ns3119889.ip-51-38-181.eu:8443/svn/oasis@380 24132e46-67f6-c34f-8568-d0b77f9b99e6

---
+++ OasisAdmini/Form/FrmAdministrateur.vb
@@ -0,0 +1,29 @@
+﻿Imports Oasis_Common
+Public Class FrmAdministrateur
+
+    Public Sub New()
+
+        ' Cet appel est requis par le concepteur.
+        InitializeComponent()
+
+        ' Ajoutez une initialisation quelconque après l'appel InitializeComponent().
+        afficheTry()
+    End Sub
+
+    Private Sub BtnDebloque_Click(sender As Object, e As EventArgs) Handles BtnDebloque.Click
+        If TxtPassword.Text <> "Oasis-689" Then
+            MsgBox("Mot de passe admin incorrect")
+            Return
+        End If
+        MsgBox("Poste débloqué")
+        afficheTry()
+
+    End Sub
+
+    Private Sub afficheTry()
+        Dim nb = ReadPermTry()
+        LblNbTry.Text = "Nbre essai(s) en cours : " & nb & "/" & MAX_TRY
+        LblNbTry.ForeColor = If(nb >= MAX_TRY, Color.LightSalmon, Color.White)
+    End Sub
+
+End Class
