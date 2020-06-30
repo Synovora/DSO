f46d89b8d2e8c1db2420650e1cb42842c47aa30a
p.theis <p.theis@24132e46-67f6-c34f-8568-d0b77f9b99e6>
2020-06-30 13:03:05 +0000

    git-svn-id: https://ns3119889.ip-51-38-181.eu:8443/svn/oasis/branches/autosuivi@467 24132e46-67f6-c34f-8568-d0b77f9b99e6

--- oasis/Bean/Ordonnance/Ordonnance.vb
+++ oasis/Bean/Ordonnance/Ordonnance.vb
@@ -1,126 +1,18 @@
 ﻿Imports System.IO
 
 Public Class Ordonnance
-    Private _id As Long
-    Private _patientId As Long
-    Private _episodeId As Long
-    Private _utilisateurCreation As Long
-    Private _dateCreation As Date
-    Private _dateValidation As Date
-    Private _userValidation As Long
-    Private _dateEdition As Date
-    Private _commentaire As String
-    Private _renouvellement As Integer
-    Private _inactif As Boolean
-    Private _signature As String
-
-    Public Property Id As Long
-        Get
-            Return _id
-        End Get
-        Set(value As Long)
-            _id = value
-        End Set
-    End Property
-
-    Public Property PatientId As Long
-        Get
-            Return _patientId
-        End Get
-        Set(value As Long)
-            _patientId = value
-        End Set
-    End Property
-
-    Public Property EpisodeId As Long
-        Get
-            Return _episodeId
-        End Get
-        Set(value As Long)
-            _episodeId = value
-        End Set
-    End Property
-
-    Public Property UtilisateurCreation As Long
-        Get
-            Return _utilisateurCreation
-        End Get
-        Set(value As Long)
-            _utilisateurCreation = value
-        End Set
-    End Property
-
-    Public Property DateCreation As Date
-        Get
-            Return _dateCreation
-        End Get
-        Set(value As Date)
-            _dateCreation = value
-        End Set
-    End Property
-
-    Public Property DateValidation As Date
-        Get
-            Return _dateValidation
-        End Get
-        Set(value As Date)
-            _dateValidation = value
-        End Set
-    End Property
-
-    Public Property DateEdition As Date
-        Get
-            Return _dateEdition
-        End Get
-        Set(value As Date)
-            _dateEdition = value
-        End Set
-    End Property
-
-    Public Property Commentaire As String
-        Get
-            Return _commentaire
-        End Get
-        Set(value As String)
-            _commentaire = value
-        End Set
-    End Property
-
-    Public Property Renouvellement As Integer
-        Get
-            Return _renouvellement
-        End Get
-        Set(value As Integer)
-            _renouvellement = value
-        End Set
-    End Property
-
-    Public Property UserValidation As Long
-        Get
-            Return _userValidation
-        End Get
-        Set(value As Long)
-            _userValidation = value
-        End Set
-    End Property
-
-    Public Property Inactif As Boolean
-        Get
-            Return _inactif
-        End Get
-        Set(value As Boolean)
-            _inactif = value
-        End Set
-    End Property
-
-    Public Property Signature As String
-        Get
-            Return _signature
-        End Get
-        Set(value As String)
-            _signature = value
-        End Set
-    End Property
+    Property Id As Long
+    Property PatientId As Long
+    Property EpisodeId As Long
+    Property UtilisateurCreation As Long
+    Property DateCreation As Date
+    Property DateValidation As Date
+    Property UserValidation As Long
+    Property DateEdition As Date
+    Property Commentaire As String
+    Property Renouvellement As Integer
+    Property Inactif As Boolean
+    Property Signature As String
 
     Public Function Clone() As Ordonnance
         Dim newInstance As Ordonnance = DirectCast(Me.MemberwiseClone(), Ordonnance)
@@ -130,16 +22,16 @@ Public Class Ordonnance
     Public Function Serialize() As Byte()
         Using m As MemoryStream = New MemoryStream()
             Using writer As BinaryWriter = New BinaryWriter(m)
-                writer.Write(_id) 'Long
-                writer.Write(_patientId) 'Long
-                writer.Write(_episodeId) 'Long
-                writer.Write(_utilisateurCreation) 'Long
-                writer.Write(_dateCreation.Ticks) 'Date -> Long
-                writer.Write(_dateValidation.Ticks) 'Date -> Long
-                writer.Write(_userValidation) 'Long
+                writer.Write(Id) 'Long
+                writer.Write(PatientId) 'Long
+                writer.Write(EpisodeId) 'Long
+                writer.Write(UtilisateurCreation) 'Long
+                writer.Write(DateCreation.Ticks) 'Date -> Long
+                writer.Write(DateValidation.Ticks) 'Date -> Long
+                writer.Write(UserValidation) 'Long
                 'writer.Write(_dateEdition.Ticks) 'Date -> LongK
-                writer.Write(_commentaire) 'String
-                writer.Write(_renouvellement) 'Int
+                writer.Write(Commentaire) 'String
+                writer.Write(Renouvellement) 'Int
             End Using
             Return m.ToArray()
         End Using
@@ -158,7 +50,7 @@ Public Class Ordonnance
                 result.UserValidation = reader.ReadInt64()
                 'result.DateEdition = New Date(reader.ReadInt64())
                 result.Commentaire = reader.ReadString()
-                result.Renouvellement = reader.ReadInt32()
+                result.Renouevellement = reader.ReadInt32()
             End Using
         End Using
         Return result
--- oasis/Bean/Ordonnance/OrdonnanceDetail.vb
+++ oasis/Bean/Ordonnance/OrdonnanceDetail.vb
@@ -1,306 +1,36 @@
 ﻿Imports System.IO
 
 Public Class OrdonnanceDetail
-    Private _ligneId As Integer
-    Private _OrdonnanceId As Integer
-    Private _traitement As Boolean
-    Private _TraitementId As Integer
-    Private _ald As Boolean
-    Private _aDelivrer As Boolean
-    Private _OrdreAffichage As Integer
-    Private _medicamentCis As Integer
-    Private _medicamentDci As String
-    Private _dateDebut As Date
-    Private _dateFin As Date
-    Private _duree As Integer
-    Private _posologie As String
-    Private _posologieBase As String
-    Private _posologieRythme As Integer
-    Private _posologieMatin As Integer
-    Private _posologieMidi As Integer
-    Private _posologieApresMidi As Integer
-    Private _posologieSoir As Integer
-    Private _fractionMatin As String
-    Private _fractionMidi As String
-    Private _fractionApresMidi As String
-    Private _fractionSoir As String
-    Private _posologieCommentaire As String
-    Private _Commentaire As String
-    Private _fenetre As Boolean
-    Private _fenetreDateDebut As Date
-    Private _fenetreDateFin As Date
-    Private _fenetreCommentaire As String
-    Private _inactif As Boolean
-
-    Public Property LigneId As Integer
-        Get
-            Return _ligneId
-        End Get
-        Set(value As Integer)
-            _ligneId = value
-        End Set
-    End Property
-
-    Public Property OrdonnanceId As Integer
-        Get
-            Return _OrdonnanceId
-        End Get
-        Set(value As Integer)
-            _OrdonnanceId = value
-        End Set
-    End Property
-
-    Public Property Traitement As Boolean
-        Get
-            Return _traitement
-        End Get
-        Set(value As Boolean)
-            _traitement = value
-        End Set
-    End Property
-
-    Public Property TraitementId As Integer
-        Get
-            Return _TraitementId
-        End Get
-        Set(value As Integer)
-            _TraitementId = value
-        End Set
-    End Property
-
-    Public Property OrdreAffichage As Integer
-        Get
-            Return _OrdreAffichage
-        End Get
-        Set(value As Integer)
-            _OrdreAffichage = value
-        End Set
-    End Property
-
-    Public Property MedicamentCis As Integer
-        Get
-            Return _medicamentCis
-        End Get
-        Set(value As Integer)
-            _medicamentCis = value
-        End Set
-    End Property
-
-    Public Property MedicamentDci As String
-        Get
-            Return _medicamentDci
-        End Get
-        Set(value As String)
-            _medicamentDci = value
-        End Set
-    End Property
-
-    Public Property DateDebut As Date
-        Get
-            Return _dateDebut
-        End Get
-        Set(value As Date)
-            _dateDebut = value
-        End Set
-    End Property
-
-    Public Property DateFin As Date
-        Get
-            Return _dateFin
-        End Get
-        Set(value As Date)
-            _dateFin = value
-        End Set
-    End Property
-
-    Public Property PosologieBase As String
-        Get
-            Return _posologieBase
-        End Get
-        Set(value As String)
-            _posologieBase = value
-        End Set
-    End Property
-
-    Public Property PosologieRythme As Integer
-        Get
-            Return _posologieRythme
-        End Get
-        Set(value As Integer)
-            _posologieRythme = value
-        End Set
-    End Property
-
-    Public Property PosologieMatin As Integer
-        Get
-            Return _posologieMatin
-        End Get
-        Set(value As Integer)
-            _posologieMatin = value
-        End Set
-    End Property
-
-    Public Property PosologieMidi As Integer
-        Get
-            Return _posologieMidi
-        End Get
-        Set(value As Integer)
-            _posologieMidi = value
-        End Set
-    End Property
-
-    Public Property PosologieApresMidi As Integer
-        Get
-            Return _posologieApresMidi
-        End Get
-        Set(value As Integer)
-            _posologieApresMidi = value
-        End Set
-    End Property
-
-    Public Property PosologieSoir As Integer
-        Get
-            Return _posologieSoir
-        End Get
-        Set(value As Integer)
-            _posologieSoir = value
-        End Set
-    End Property
-
-    Public Property Commentaire As String
-        Get
-            Return _Commentaire
-        End Get
-        Set(value As String)
-            _Commentaire = value
-        End Set
-    End Property
-
-    Public Property Fenetre As Boolean
-        Get
-            Return _fenetre
-        End Get
-        Set(value As Boolean)
-            _fenetre = value
-        End Set
-    End Property
-
-    Public Property FenetreDateDebut As Date
-        Get
-            Return _fenetreDateDebut
-        End Get
-        Set(value As Date)
-            _fenetreDateDebut = value
-        End Set
-    End Property
-
-    Public Property FenetreDateFin As Date
-        Get
-            Return _fenetreDateFin
-        End Get
-        Set(value As Date)
-            _fenetreDateFin = value
-        End Set
-    End Property
-
-    Public Property FenetreCommentaire As String
-        Get
-            Return _fenetreCommentaire
-        End Get
-        Set(value As String)
-            _fenetreCommentaire = value
-        End Set
-    End Property
-
-    Public Property Inactif As Boolean
-        Get
-            Return _inactif
-        End Get
-        Set(value As Boolean)
-            _inactif = value
-        End Set
-    End Property
-
-    Public Property PosologieCommentaire As String
-        Get
-            Return _posologieCommentaire
-        End Get
-        Set(value As String)
-            _posologieCommentaire = value
-        End Set
-    End Property
-
-    Public Property Ald As Boolean
-        Get
-            Return _ald
-        End Get
-        Set(value As Boolean)
-            _ald = value
-        End Set
-    End Property
-
-    Public Property FractionMatin As String
-        Get
-            Return _fractionMatin
-        End Get
-        Set(value As String)
-            _fractionMatin = value
-        End Set
-    End Property
-
-    Public Property FractionMidi As String
-        Get
-            Return _fractionMidi
-        End Get
-        Set(value As String)
-            _fractionMidi = value
-        End Set
-    End Property
-
-    Public Property FractionApresMidi As String
-        Get
-            Return _fractionApresMidi
-        End Get
-        Set(value As String)
-            _fractionApresMidi = value
-        End Set
-    End Property
-
-    Public Property FractionSoir As String
-        Get
-            Return _fractionSoir
-        End Get
-        Set(value As String)
-            _fractionSoir = value
-        End Set
-    End Property
-
-    Public Property ADelivrer As Boolean
-        Get
-            Return _aDelivrer
-        End Get
-        Set(value As Boolean)
-            _aDelivrer = value
-        End Set
-    End Property
-
-    Public Property Duree As Integer
-        Get
-            Return _duree
-        End Get
-        Set(value As Integer)
-            _duree = value
-        End Set
-    End Property
-
-    Public Property Posologie As String
-        Get
-            Return _posologie
-        End Get
-        Set(value As String)
-            _posologie = value
-        End Set
-    End Property
+    Property LigneId As Integer
+    Property OrdonnanceId As Integer
+    Property Traitement As Boolean
+    Property TraitementId As Integer
+    Property Ald As Boolean
+    Property ADelivrer As Boolean
+    Property OrdreAffichage As Integer
+    Property MedicamentCis As Integer
+    Property MedicamentDci As String
+    Property DateDebut As Date
+    Property DateFin As Date
+    Property Duree As Integer
+    Property Posologie As String
+    Property PosologieBase As String
+    Property PosologieRythme As Integer
+    Property PosologieMatin As Integer
+    Property PosologieMidi As Integer
+    Property PosologieApresMidi As Integer
+    Property PosologieSoir As Integer
+    Property FractionMatin As String
+    Property FractionMidi As String
+    Property FractionApresMidi As String
+    Property FractionSoir As String
+    Property PosologieCommentaire As String
+    Property Commentaire As String
+    Property Fenetre As Boolean
+    Property FenetreDateDebut As Date
+    Property FenetreDateFin As Date
+    Property FenetreCommentaire As String
+    Property Inactif As Boolean
 
     Public Function Clone() As OrdonnanceDetail
         Dim newInstance As OrdonnanceDetail = DirectCast(Me.MemberwiseClone(), OrdonnanceDetail)
@@ -311,34 +41,34 @@ Public Class OrdonnanceDetail
         Using m As MemoryStream = New MemoryStream()
             Using writer As BinaryWriter = New BinaryWriter(m)
                 'writer.Write(_ligneId) 'Int
-                writer.Write(_OrdonnanceId) 'Int
-                writer.Write(_traitement) 'Bool
-                writer.Write(_TraitementId) 'Int
-                writer.Write(_ald) 'Bool
-                writer.Write(_aDelivrer) 'Bool
-                writer.Write(_OrdreAffichage) 'Int
-                writer.Write(_medicamentCis) 'Int
-                writer.Write(If(_medicamentDci, "")) 'String
-                writer.Write(_dateDebut.Ticks) 'Date -> Long
-                writer.Write(_dateFin.Ticks) 'Date -> Long
-                writer.Write(_duree) 'Int
-                writer.Write(If(_posologie, "")) 'String
-                writer.Write(If(_posologieBase, "")) 'String
-                writer.Write(_posologieRythme) 'Int
-                writer.Write(_posologieMatin) 'Int
-                writer.Write(_posologieMidi) 'Int
-                writer.Write(_posologieApresMidi) 'Int
-                writer.Write(_posologieSoir) 'Int
-                writer.Write(_fractionMatin) 'String
-                writer.Write(_fractionMidi) 'String
-                writer.Write(If(_fractionApresMidi, "")) 'String
-                writer.Write(_fractionSoir) 'String
-                writer.Write(If(_posologieCommentaire, "")) 'String
-                writer.Write(If(_Commentaire, "")) 'String
-                writer.Write(_fenetre) 'Bool
-                writer.Write(_fenetreDateDebut.Ticks) 'Date -> Long
-                writer.Write(_fenetreDateFin.Ticks) 'Date -> Long
-                writer.Write(If(_fenetreCommentaire, "")) 'String
+                writer.Write(OrdonnanceId) 'Int
+                writer.Write(Traitement) 'Bool
+                writer.Write(TraitementId) 'Int
+                writer.Write(Ald) 'Bool
+                writer.Write(ADelivrer) 'Bool
+                writer.Write(OrdreAffichage) 'Int
+                writer.Write(MedicamentCis) 'Int
+                writer.Write(If(MedicamentDci, "")) 'String
+                writer.Write(DateDebut.Ticks) 'Date -> Long
+                writer.Write(DateFin.Ticks) 'Date -> Long
+                writer.Write(Duree) 'Int
+                writer.Write(If(Posologie, "")) 'String
+                writer.Write(If(PosologieBase, "")) 'String
+                writer.Write(PosologieRythme) 'Int
+                writer.Write(PosologieMatin) 'Int
+                writer.Write(PosologieMidi) 'Int
+                writer.Write(PosologieApresMidi) 'Int
+                writer.Write(PosologieSoir) 'Int
+                writer.Write(FractionMatin) 'String
+                writer.Write(FractionMidi) 'String
+                writer.Write(If(FractionApresMidi, "")) 'String
+                writer.Write(FractionSoir) 'String
+                writer.Write(If(PosologieCommentaire, "")) 'String
+                writer.Write(If(Commentaire, "")) 'String
+                writer.Write(Fenetre) 'Bool
+                writer.Write(FenetreDateDebut.Ticks) 'Date -> Long
+                writer.Write(FenetreDateFin.Ticks) 'Date -> Long
+                writer.Write(If(FenetreCommentaire, "")) 'String
             End Using
             Return m.ToArray()
         End Using
